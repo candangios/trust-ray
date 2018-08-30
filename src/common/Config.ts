b850070601da0c01bddfd099e320af18148d4025
Can dang <candd1707@gmail.com>
2018-08-30 15:19:33 +0700

    add rinkeby network

--- src/common/Config.ts
+++ src/common/Config.ts
@@ -2,6 +2,6 @@ const Web3 = require("web3");
 const config = require("config");
 
 export class Config {
-        static network = config.get("RPC_SERVER");
+        static network = config.get("https://rinkeby.infura.io/v3/a78f819911994678934d1c811f3c4b47");
         static web3 = new Web3(new Web3.providers.HttpProvider(Config.network));
 }
